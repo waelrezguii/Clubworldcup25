9c57000e851f2b7cdd909a125c887db7ad006b29
Wael <wael.rezgui@esen.tn>
2024-09-07 00:27:38 +0100

    ers

--- src/app/teams.service.ts
+++ src/app/teams.service.ts
@@ -8,11 +8,11 @@ import { Teams } from './models/team.model';
   providedIn: 'root'
 })
 export class TeamsService {
-  private baseUrl = 'https://api.render.com/srv-crd40f3tq21c73ct2v7g/api/v1/teams';
+  private apiUrl = 'https://api.render.com/srv-crd40f3tq21c73ct2v7g/api/v1/teams';
 
   constructor(private http: HttpClient) { }
 
   getTeams(): Observable<Teams[]> {
-    return this.http.get<Teams[]>(this.baseUrl);
+    return this.http.get<Teams[]>(this.apiUrl);
   }
 }
